99a123aba847164c066216aed5a2b76c534561d6
Sofiia <sofiia@local>
2026-05-09 11:58:30 +0200

    Reset main to blank readme

--- src/Bookstore.Domain/Entities/ProductAuthorLink.cs
+++
@@ -1,8 +0,0 @@
-namespace Bookstore.Domain.Entities;
-
-public class ProductAuthorLink
-{
-    public Guid ProductId { get; set; }
-    public string AuthorId { get; set; } = string.Empty;
-    public DateTime CachedAt { get; set; }
-}
